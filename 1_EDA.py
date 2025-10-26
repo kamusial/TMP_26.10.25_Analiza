c503b04a3c110e2221cf4ae39fee7b2e4f16c7ac
student-wroclaw <student-wroclaw@EDU-WSB.local>
2025-10-26 09:47:17 +0100

    2-dane

--- 1_EDA.py
+++ 1_EDA.py
@@ -1,1 +1,25 @@
-..
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+# 1. Pobieranie danych
+
+# df = pd.read_csv('dane_csv.csv', delimiter=None, header='infer', index_col=None, usecols=None, dtype=None, engine=None, converters=None, true_values=None, false_values=None, skipinitialspace=False, skiprows=None, skipfooter=0, nrows=None, na_values=None, keep_default_na=True, na_filter=True, skip_blank_lines=True, parse_dates=None, date_format=None, dayfirst=False, cache_dates=True, iterator=False, chunksize=None, compression='infer', thousands=None, decimal='.', lineterminator=None, quotechar='"', quoting=0, doublequote=True, escapechar=None, comment=None, encoding=None, encoding_errors='strict', dialect=None, on_bad_lines='error', low_memory=True, memory_map=False, float_precision=None, storage_options=None)
+
+url = "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-31/beers.csv"
+
+try:
+    df = pd.read_csv(url)
+    print('dane pobrane')
+except Exception as e:
+    print(f'Błąd {e}')
+    print('Używam danych zapasowych')
+    data = {
+        'nazwa': ['IPA','IPA', 'Lager', 'Stout', 'Pilsner', 'Wheat', 'Porter', 'Ale', 'Bock'],
+        'alkohol': [6.5, 6.5, 5.0, 7.2, 4.8, 5.2, 5.8, 5.5, 6.8],
+        'goryczka': [65, 65, np.nan, 45, 30, 15, 40, 35, 25],
+        'ocena': [4.2, 4.2, 3.8, 4.5, 3.9, 3.7, 4.1, 4.0, 4.3],
+        'styl': ['IPA', 'IPA', 'Lager', 'Ciemne', 'Lager', np.nan, 'Ciemne', 'Jasne', 'Ciemne']
+    }
+    df = pd.DataFrame(data)
